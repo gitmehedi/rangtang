5233605eb514e0aeb88b757da4d909d2b385a167
gitmehedi <git.mehedi@gmail.com>
2015-03-07 19:07:41 +0600

    add new files

---
+++ public/src/result.php
@@ -0,0 +1,15 @@
+<?php
+$newArray = array(
+    "id" => "Mango-1",
+    "id" => "Mango-2",
+    "id" => "Mango-3",
+    "id" => "Mango-4",
+    "id" => "Mango-5",
+    "id" => "Mango-6",
+    "id" => "Mango-7",
+    "id" => "Mango-8",
+
+
+);
+
+echo json_encode($newArray);
